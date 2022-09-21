44e2c18d3baa986a94b7d05be1dbd0f096dcf88f
Jefferson Bruno <bruunosoousaa@gmail.com>
2022-09-21 15:51:33 -0300

    primeiro exercicio js

---
+++ fundamentos/04-Introducao-a-java-script-e-logica/01-JS-primeiros-passos/scripts.js
@@ -0,0 +1,10 @@
+const myName = "jefferson";
+const birthCity = "Campina";
+let birthYear = 94;
+
+birthYear = 2030;
+birthCity = "JP";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
