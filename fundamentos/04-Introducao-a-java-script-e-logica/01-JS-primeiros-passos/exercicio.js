ace0ac4d375371b02e0d93ff1d8ab5771c03cbd3
Jefferson Bruno <bruunosoousaa@gmail.com>
2022-09-22 15:25:07 -0300

    questão 5 do exercicio

--- fundamentos/04-Introducao-a-java-script-e-logica/01-JS-primeiros-passos/exercicio.js
+++ fundamentos/04-Introducao-a-java-script-e-logica/01-JS-primeiros-passos/exercicio.js
@@ -35,3 +35,20 @@ if(teste > 0) {
 }else {
     console.log("Zero")
 }
+
+let angulo1 = 60;
+let angulo2 = 60;
+let angulo3 = 60;
+
+let somaAngulos = angulo1 + angulo2 + angulo3;
+let angulos = angulo1 > 0 && angulo2 > 0 && angulo3 > 0;
+
+if(angulos) {
+    if(somaAngulos === 180){
+        console.log("True");
+    }else{
+        console.log("False");
+    }
+} else {
+    console.log("os ângulos não formam um triângulo");
+}
