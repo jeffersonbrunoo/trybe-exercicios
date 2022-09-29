2c04ebfac13ffaa2bfe274f2fc618b2dc3110132
Jefferson Bruno <bruunosoousaa@gmail.com>
2022-09-29 16:25:23 -0300

    seletores

--- fundamentos/04-Introducao-a-java-script-e-logica/05-DOM-EVENTOS-WEB-STORAGE/SELETORES/script.js
+++ fundamentos/04-Introducao-a-java-script-e-logica/05-DOM-EVENTOS-WEB-STORAGE/SELETORES/script.js
@@ -1,2 +1,10 @@
 const header = document.getElementById('header-container');
 header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+const selection = document.getElementsByClassName("emergency-tasks")[0];
+selection.style.backgroundColor = 'rgb(255, 159, 132)';
+
+const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+  emergencyTasksHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
+}
