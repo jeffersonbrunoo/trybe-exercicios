80448b91718547989a376b00a243d529d21f390b
Jefferson Bruno <bruunosoousaa@gmail.com>
2022-09-29 18:13:24 -0300

    seletores

--- fundamentos/04-Introducao-a-java-script-e-logica/05-DOM-EVENTOS-WEB-STORAGE/SELETORES/script.js
+++ fundamentos/04-Introducao-a-java-script-e-logica/05-DOM-EVENTOS-WEB-STORAGE/SELETORES/script.js
@@ -8,3 +8,12 @@ const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
 for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
   emergencyTasksHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
 }
+const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+noEmergencyTasks.style.backgroundColor = 'rgb(249, 219, 94)';
+
+const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+  noEmergencyTasksHeaders[index].style.backgroundColor = 'rgb(35, 37, 37)';
+}
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'rgb(0, 53, 51)';
