e6cd89f250c7649c4faf678876a53f96b1445c3d
Jefferson Bruno <bruunosoousaa@gmail.com>
2022-10-24 15:17:05 -0300

    fluxos de excecoes

--- fundamentos/07-ES6 E TESTES UNITARIOS/let-var-const-arrow-functions-template-literals/script.js
+++ fundamentos/07-ES6 E TESTES UNITARIOS/let-var-const-arrow-functions-template-literals/script.js
@@ -98,3 +98,69 @@ return resultado;
 };
 
 console.log(minhasSkills(substituaX('bebeto')));
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arrayOfSkills = Object.keys(student);
+  for (const index in arrayOfSkills) {
+    console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+  }
+};
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
+
+const coolestTvShow = {
+  name: 'BoJack Horseman',
+  genre: 'adult animation',
+  createdBy: 'Raphael Bob-Waksberg',
+  favoriteCharacter: 'Princess Carolyn',
+  quote: 'Princess Carolyn always lands on her feet.',
+  seasons: 6,
+};
+
+console.log(Object.entries(coolestTvShow));
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
+
+const band = {
+  name: 'Blind Guardian',
+  formedIn: 1986,
+  lastAlbum: 'Beyond the Red Mirror',
+};
+
+const info = {
+  genre: 'Power Metal',
+  lastAlbum: 'Twilight Orchestra: Legacy of the Dark Lands',
+};
+
+console.log(Object.assign(band, info));
