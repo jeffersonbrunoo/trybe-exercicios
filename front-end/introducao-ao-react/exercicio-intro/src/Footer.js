788f00e1532e0e5412cd511c54fc02230d4ea1d3
Jefferson Bruno <bruunosoousaa@gmail.com>
2022-11-29 16:20:49 -0300

    exercicio react 1

---
+++ front-end/introducao-ao-react/exercicio-intro/src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <h3>Isso é só o começo...</h3>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
---
+++ front-end/introducao-ao-react/exercicio-intro/src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Conteúdos de Frontend</h1>
+      </div>
+    )
+  }
+}
+
+export default Header;
